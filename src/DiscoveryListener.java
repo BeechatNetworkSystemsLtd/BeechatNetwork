d3e108884fbddce923e65a383ed805ebe915ef49
Christian Hollinger <christian.hollinger2003@gmail.com>
2020-05-27 12:44:55 -0400

    YES YES YES GO COCAINEEEEE

--- src/DiscoveryListener.java
+++ src/DiscoveryListener.java
@@ -4,6 +4,7 @@ import com.digi.xbee.api.RemoteXBeeDevice;
 import com.digi.xbee.api.listeners.IDiscoveryListener;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DiscoveryListener implements IDiscoveryListener {
 
@@ -15,9 +16,14 @@ public class DiscoveryListener implements IDiscoveryListener {
     DiscoveryListener(DigiMeshNetwork network, DigiMeshDevice localDevice){
         this.network = network;
         this.localDevice = localDevice;
+
         System.out.println("Starting discovery process, please wait...");
 
-        frame.add(new JLabel("Discovering devices in network.\nThis should take about 15 seconds"));
+        frame.setLayout(new BorderLayout());
+        frame.setMinimumSize(new Dimension(300,400));
+        frame.add(new JLabel("Discovering devices in network.\nThis should only take a moment."),
+                BorderLayout.NORTH);
+        frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         frame.setVisible(true);
 
@@ -27,18 +33,30 @@ public class DiscoveryListener implements IDiscoveryListener {
     public void deviceDiscovered(RemoteXBeeDevice remoteXBeeDevice) {
         System.out.println("Discovered device " + remoteXBeeDevice.getNodeID());
         frame.add(new JLabel("Discovered device: " + remoteXBeeDevice.getNodeID()));
+        frame.pack();
         network.addRemoteDevice(remoteXBeeDevice);
     }
 
     @Override
     public void discoveryError(String s) {
         frame.add(new JLabel("Experienced an error: " + s));
+        frame.pack();
         System.err.println(s);
     }
 
     @Override
     public void discoveryFinished(String s) {
-        System.out.println("Discovery finished. Message: " + s);
+
+        frame.dispose();
+
+        if (s == null) {
+            JOptionPane.showMessageDialog(null, "Done! " + network.getNumberOfDevices() +
+                    " devices found.", "Discovery finished", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Done! " + network.getNumberOfDevices() +
+                    " devices found.\n" + s, "Discovery Finished", JOptionPane.INFORMATION_MESSAGE);
+        }
+
         new NetworkView(network, localDevice);
     }
 }
