6e4fa494b3091290631bccfe4cbff40368527929
BeeChatNetworkAdmin <beechatnetwork@gmail.com>
2020-06-06 19:15:34 +0200

    Chat fully working.

--- src/CreateFile.java
+++ src/CreateFile.java
@@ -2,13 +2,18 @@ import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 
 public class CreateFile {
-    public CreateFile(String filepath) {
+    public CreateFile(String filepath, boolean overwrite) {
         try {
             File myObj = new File(filepath);
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
             } else {
                 System.out.println("File already exists.");
+                if (overwrite){
+                    myObj.delete();
+                    myObj.createNewFile();
+                    System.out.println("Overwritten "+filepath);
+                }
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
--- src/Decrypt.java
+++ src/Decrypt.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Decrypt {
     public Decrypt(String filepath, String sharedsecret) throws InterruptedException, IOException {
         Process decwithSSP = Runtime.getRuntime().exec(new String[]{
-                "bash", "-c", "openssl enc -d -aes-256-cbc -in " + filepath + " -out " + System.getProperty("user.dir") + "/" + filepath.substring(0,filepath.lastIndexOf(".bin")) + " -pass pass:" + sharedsecret});
+                "bash", "-c", "openssl enc -d -aes-256-cbc -in " + filepath + " -out " + filepath.substring(0,filepath.lastIndexOf(".bin")) + " -pass pass:" + sharedsecret});
         decwithSSP.waitFor();
     }
 }
