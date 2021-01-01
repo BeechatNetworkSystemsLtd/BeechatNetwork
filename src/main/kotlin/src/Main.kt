61d6f7da42ce87b96ab2f097fa60ac66ada36a89
christian <christian.hollinger2003@gmail.com>
2021-01-01 13:24:02 -0500

    Added copyright notices and IntelliJ copyright profile

--- src/main/kotlin/src/Main.kt
+++ src/main/kotlin/src/Main.kt
@@ -1,3 +1,22 @@
+/*
+ * This file is part of BeeChat.
+ *
+ *     BeeChat is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     BeeChat is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with BeeChat.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+
 package src
 
 import javafx.application.Application
@@ -6,12 +25,13 @@ import kotlin.jvm.JvmStatic
 
 class Main : Application() {
     override fun start(primaryStage: Stage) {
-
     }
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            // Display copyright notice
+
             launch(*args)
         }
     }
